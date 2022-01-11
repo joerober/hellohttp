891365f1adc4d77344099f982353f2235e2d270c
Joel Roberts <joel.robertsjr@gmail.com>
2022-01-10 21:50:55 -0700

    main.go edit

--- main.go
+++ main.go
@@ -2,16 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 // simple hello http func
 func hellohttp(w http.ResponseWriter, _ *http.Request) {
 	fmt.Fprintf(w, "hello http!")
+	log.Println("hello http")
 }
 
 func main() {
 	http.HandleFunc("/", hellohttp)
+
+	// func as input value
+	http.HandleFunc("/goodbye/", func(w http.ResponseWriter, _ *http.Request) {
+		fmt.Fprintf(w, "goodbye...")
+		log.Println("Goodbye http")
+	})
+
 	//no addr will listen on every interface
 	http.ListenAndServe(":9999", nil)
 }
